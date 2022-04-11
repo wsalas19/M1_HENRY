c8a4064c999498d467790c6d0bbedf29e2614127
wsalas19 <willbullet52@gmail.com>
2022-04-11 17:17:57 -0500

    CS homework done

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -1,13 +1,27 @@
 'use strict'
 
+
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let arrayBinario = num.split("").reverse().map(a => Number(a));
+  let resultado = [];
+  for (let i = 0; i < arrayBinario.length; i++) {
+    resultado.push(arrayBinario[i] * Math.pow(2, i))
+  }
+  return resultado.reduce((a, b) => (a + b))
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let binario = [];
+  let divisor = num;
 
+  while (divisor !== 0) {
+    binario.push(Math.floor(divisor % 2));
+    divisor = Math.floor(divisor / 2);
+  }
+
+  return binario.reverse().join("");
 }
 
 
