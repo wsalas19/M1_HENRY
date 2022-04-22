e96051179eb861221aaab1e1f1a5fdebc0a0dc0b
wsalas19 <willbullet52@gmail.com>
2022-04-22 12:26:48 -0500

    Algoritmos II homework done

--- 06-Algoritmos-I/homework/homework.js
+++ 06-Algoritmos-I/homework/homework.js
@@ -14,7 +14,16 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < (array.length - i - 1); j++) {
+      if (array[j] > array[j + 1]) {
+        var temp = array[j]
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,7 +32,22 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  /*
+  empiezo a recorrer el arreglo
+  selecciono el primer elemento y lo voy comparando con el anterior
+  comparacion : revisa si el seleccionado es menor 
+  */
 
+  for (var i = 1; i < array.length; i++) {
+    let m = i - 1;
+    let aux = array[i];
+    while (m >= 0 && array[m] > aux) {
+      array[m + 1] = array[m];
+      m--;
+    }
+    array[m + 1] = aux
+  }
+  return array;
 }
 
 
@@ -32,6 +56,8 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+
+
 
 }
 
--- 07-Algoritmos-II/homework/homework.js
+++ 07-Algoritmos-II/homework/homework.js
@@ -6,7 +6,24 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length <= 1) return array;
 
+  let pivot = array[Math.floor(Math.random() * array.length)];
+  let left = [];
+  let right = [];
+  let equal = [];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else if (array[i] > pivot) {
+      right.push(array[i]);
+    } else {
+      equal.push(array[i]);
+    }
+  }
+  return quickSort(left).concat(equal).concat(quickSort(right));
+    
 }
 
 function mergeSort(array) {
@@ -14,7 +31,33 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if (array.length === 1) return array;
+  let div = dividir(array);
+  let left = div[0];
+  let right = div[1];
+return unir(mergeSort(left), mergeSort(right)); 
 
+}
+function dividir(array) {
+  let mitad = Math.floor(array.length / 2);
+  let left = array.slice(0, mitad);
+  let right = array.slice(mitad);
+  return [left, right];
+}
+function unir(left, right) {
+  let array = [];
+  let lftIndex = 0;
+  let rghtIndex = 0;
+  while (lftIndex < left.length && rghtIndex < right.length) {
+    if (left[lftIndex] < right[rghtIndex]) {
+      array.push(left[lftIndex]);
+      lftIndex++;
+    } else {
+      array.push(right[rghtIndex]);
+      rghtIndex++;
+    }
+  }
+  return array.concat(left.slice(lftIndex)).concat(right.slice(rghtIndex));
 }
 
 // No modificar nada debajo de esta línea
